65730271b8c216e1dd6faee0f5fde39f9386d9d4
Arun Michael Dsouza <amdsouza92@gmail.com>
2013-10-12 02:40:25 +0530

    System snap header added

---
+++ sys_snap.h
@@ -0,0 +1,25 @@
+/* Header for various system date/time/day functions.
+
+   sys_date_def()  - Displays the system date in default format.
+   sys_day()       - Displays the current system calendar day.
+   sys_day_info()  - Displays the complete current system calendar day info.
+   sys_time_12hr() - Displays the system time in 12hr format.
+   sys_time_24hr() - Displays the system time in 24hr format.
+   sys_time_def()  - Displays the system time in default format.
+
+   Coding by - Arun Michael Dsouza
+   Date : 12/10/2013
+
+*/
+
+#ifndef SYS_SNAP_H_INCLUDED
+#define SYS_SNAP_H_INCLUDED
+
+void sys_date_def();
+void sys_day();
+void sys_day_info();
+void sys_time_12hr();
+void sys_time_24hr();
+void sys_time_def();
+
+#endif // SYS_SNAP_H_INCLUDED
