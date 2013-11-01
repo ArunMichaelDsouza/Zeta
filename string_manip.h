4dde4ebd12558d509f327c17b30914905d47f731
Arun Michael Dsouza <amdsouza92@gmail.com>
2013-11-02 01:52:27 +0530

    String Manipulation Library added

---
+++ string_manip.h
@@ -0,0 +1,15 @@
+/* Header for various string manipulation functions.
+
+   strln() - Returns the length of the entered string.
+
+   Coding by - Arun Michael Dsouza
+   Date : 02/11/2013
+
+*/
+
+#ifndef STRING_MANIP_H_INCLUDED
+#define STRING_MANIP_H_INCLUDED
+
+void strln();
+
+#endif // STRING_MANIP_H_INCLUDED
