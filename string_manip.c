60e423f2e94a71b49e9f8804641b7ee72b1f98c7
Arun Michael Dsouza <amdsouza92@gmail.com>
2013-11-14 01:16:10 +0530

    String Manipulation Source file updated!

--- string_manip.c
+++ string_manip.c
@@ -1,6 +1,7 @@
 /* Definition of various string manipulation functions.
 
-   strln() - Returns the length of the entered string.
+   strln()  - Returns the length of the entered string.
+   strwht() - Returns the no. of whitespaces in the entered string.
 
    Coding by - Arun Michael Dsouza
    Date : 02/11/2013
@@ -10,13 +11,16 @@
 #include "string_manip.h"
 #include <stdio.h>
 
+int n=1000; //Default size which can be held
+char str[1000];
+
+int i=0;
+
 void strln()
 {
-    int n=1000; //Default size which can be held
-    char str[n];
     fgets(str, n, stdin);
-    scanf("%[^\n]s",str);
-    int i=0;
+    scanf("%[^\n]s",str); // Scans the input until the new line character is encountered and stores it
+
     int len=0;
     while(str[i]!='\0')
     {
@@ -26,4 +30,30 @@ void strln()
 
   printf("%d",len);
   printf("\n");
+}
+
+void strwht()
+{
+    fgets(str, n, stdin);
+    scanf("%[^\n]s",str); // Scans the input until the new line character is encountered and stores it
+
+    int count=0;
+    int len=0;
+
+    i=0;
+    while(str[i]!='\0')
+    {
+        len++;
+        i++;
+    }
+
+    for(i=0;i<=len;i++)
+    {
+        if(str[i]==(char)32) //Simple equivalent -> if(str[i]==' ')
+        {
+            count++;
+        }
+    }
+    printf("%d",count);
+    printf("\n");
 }
