c3c60faa7fcde2dbc248655da23cdfe5a206a20f
Arun Michael Dsouza <amdsouza92@gmail.com>
2013-11-14 01:11:06 +0530

    String Manipulation Library updated!

--- string_manip.h
+++ string_manip.h
@@ -1,6 +1,7 @@
 /* Header for various string manipulation functions.
 
    strln() - Returns the length of the entered string.
+   strwht() - Returns the no. of whitespaces in the entered string.
 
    Coding by - Arun Michael Dsouza
    Date : 02/11/2013
@@ -11,5 +12,6 @@
 #define STRING_MANIP_H_INCLUDED
 
 void strln();
+void strwht();
 
 #endif // STRING_MANIP_H_INCLUDED
