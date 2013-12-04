df9936bcdcf2a159cdcf7262998deab1e0ea7a65
Arun Michael Dsouza <amdsouza92@gmail.com>
2013-12-05 00:40:19 +0530

    String Manipulation source file updated

--- src/str_manip.c
+++ src/str_manip.c
@@ -3,6 +3,7 @@
    strln()    - Returns the length of the entered string.
    strwht()   - Returns the no. of whitespaces in the entered string.
    strslice() - Returns a sub string from the entered string using the mentioned indices.
+   strcap()   - Capitalises the entered String.
 
    Coding by - Arun Michael Dsouza
    Date : 02/11/2013
@@ -33,7 +34,7 @@ void strln()
       i++;
     }
 
-    printf("%d",len);
+    printf("  %d",len);
     printf("\n");
 }
 
@@ -62,7 +63,7 @@ void strwht()
             count++;
         }
     }
-    printf("%d",count);
+    printf("  %d",count);
     printf("\n");
 }
 
@@ -90,11 +91,138 @@ void strslice()
     }
     else
     {
-    for(i=s;i<e;i++)
-    {
-        printf("%c",str[i]);
-    }
+        printf("  ");
+        for(i=s;i<e;i++)
+        {
+            printf("%c",str[i]);
+        }
     }
 
     printf("\n");
+}
+
+
+
+void strcap()
+{
+    char str[MAX];
+    int i=0;
+
+    printf("%c ",175);
+    fgets(str,sizeof(str),stdin);
+
+    for(i=0;i<strlen(str);i++)
+    {
+        switch(str[i])
+        {
+            case 'a':
+            str[i]='A';
+            break;
+
+            case 'b':
+            str[i]='B';
+            break;
+
+            case 'c':
+            str[i]='C';
+            break;
+
+            case 'd':
+            str[i]='D';
+            break;
+
+            case 'e':
+            str[i]='E';
+            break;
+
+            case 'f':
+            str[i]='F';
+            break;
+
+            case 'g':
+            str[i]='G';
+            break;
+
+            case 'h':
+            str[i]='H';
+            break;
+
+            case 'i':
+            str[i]='I';
+            break;
+
+            case 'j':
+            str[i]='J';
+            break;
+
+            case 'k':
+            str[i]='K';
+            break;
+
+            case 'l':
+            str[i]='L';
+            break;
+
+            case 'm':
+            str[i]='M';
+            break;
+
+            case 'n':
+            str[i]='N';
+            break;
+
+            case 'o':
+            str[i]='O';
+            break;
+
+            case 'p':
+            str[i]='P';
+            break;
+
+            case 'q':
+            str[i]='Q';
+            break;
+
+            case 'r':
+            str[i]='R';
+            break;
+
+            case 's':
+            str[i]='S';
+            break;
+
+            case 't':
+            str[i]='T';
+            break;
+
+            case 'u':
+            str[i]='U';
+            break;
+
+            case 'v':
+            str[i]='V';
+            break;
+
+            case 'w':
+            str[i]='W';
+            break;
+
+            case 'x':
+            str[i]='X';
+            break;
+
+            case 'y':
+            str[i]='Y';
+            break;
+
+            case 'z':
+            str[i]='Z';
+            break;
+
+            default:
+            break;
+        }
+    }
+
+    printf("  %s",str);
 }
