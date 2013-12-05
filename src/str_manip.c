ee60f806fd93de3aff99fa5e3cd42bbd16800b2d
Arun Michael Dsouza <amdsouza92@gmail.com>
2013-12-05 21:48:07 +0530

    String Manipulation source file updated

--- src/str_manip.c
+++ src/str_manip.c
@@ -4,6 +4,7 @@
    strwht()   - Returns the no. of whitespaces in the entered string.
    strslice() - Returns a sub string from the entered string using the mentioned indices.
    strcap()   - Capitalises the entered String.
+   strtolow() - Converts uppercase String to lowercase.
 
    Coding by - Arun Michael Dsouza
    Date : 02/11/2013
@@ -217,6 +218,132 @@ void strcap()
 
             case 'z':
             str[i]='Z';
+            break;
+
+            default:
+            break;
+        }
+    }
+
+    printf("  %s",str);
+}
+
+
+
+void strtolow()
+{
+    char str[MAX];
+    int i=0;
+
+    printf("%c ",175);
+    fgets(str,sizeof(str),stdin);
+
+    for(i=0;i<strlen(str);i++)
+    {
+        switch(str[i])
+        {
+            case 'A':
+            str[i]='a';
+            break;
+
+            case 'B':
+            str[i]='b';
+            break;
+
+            case 'C':
+            str[i]='c';
+            break;
+
+            case 'D':
+            str[i]='d';
+            break;
+
+            case 'E':
+            str[i]='e';
+            break;
+
+            case 'F':
+            str[i]='f';
+            break;
+
+            case 'G':
+            str[i]='g';
+            break;
+
+            case 'H':
+            str[i]='h';
+            break;
+
+            case 'I':
+            str[i]='i';
+            break;
+
+            case 'J':
+            str[i]='j';
+            break;
+
+            case 'K':
+            str[i]='k';
+            break;
+
+            case 'L':
+            str[i]='l';
+            break;
+
+            case 'M':
+            str[i]='m';
+            break;
+
+            case 'N':
+            str[i]='n';
+            break;
+
+            case 'O':
+            str[i]='o';
+            break;
+
+            case 'P':
+            str[i]='p';
+            break;
+
+            case 'Q':
+            str[i]='q';
+            break;
+
+            case 'R':
+            str[i]='r';
+            break;
+
+            case 'S':
+            str[i]='s';
+            break;
+
+            case 'T':
+            str[i]='t';
+            break;
+
+            case 'U':
+            str[i]='u';
+            break;
+
+            case 'V':
+            str[i]='v';
+            break;
+
+            case 'W':
+            str[i]='w';
+            break;
+
+            case 'X':
+            str[i]='x';
+            break;
+
+            case 'Y':
+            str[i]='y';
+            break;
+
+            case 'Z':
+            str[i]='z';
             break;
 
             default:
